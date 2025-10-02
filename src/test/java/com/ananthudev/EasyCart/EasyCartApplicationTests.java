79f1e3c048dc3776c5e6a6e2de86c50c8de22e16
Ananthu.M <ananthu.m.utr@gmail.com>
2025-10-02 22:33:37 +0530

    backend initialized with java springboot

---
+++ src/test/java/com/ananthudev/EasyCart/EasyCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ananthudev.EasyCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
